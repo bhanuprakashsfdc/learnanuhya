788f07938cbe49bd05c135b769cb1ff95baa4a61
Reddisreekanth <sreekanthreddycheepati@gmail.com>
2024-07-29 20:20:39 +0530

    pusing till eight blog

--- src/data/blogData.jsx
+++ src/data/blogData.jsx
@@ -1,21 +1,6 @@
 export const blogData = [
     {
-      title: "Salesforce Developer Edition",
-      tag: "Salesforce",
-      description: "Develop enterprise-quality apps fast with this full-featured Salesforce edition that never expires.",
-      createdDate: "2024-07-01",
-      imageUrl: "path/to/image1.jpg",
-      signupUrl: "https://developer.salesforce.com/signup"
-    },
-    {
-      title: "CRM Analytics Free Trial",
-      tag: "Salesforce",
-      description: "Infuse predictive and prescriptive insights directly into the workflow with Salesforce CRM Analytics.",
-      createdDate: "2024-06-25",
-      imageUrl: "path/to/image2.jpg",
-      signupUrl: "https://www.salesforce.com/form/signup/freetrial-crm-analytics/"
-    },
-    {
+        id: 1,
         title: "Introduction to cloud computing 101",
         tag: "Salesforce",
         description: "Develop enterprise-quality apps fast with this full-featured Salesforce edition that never expires.",
@@ -24,12 +9,70 @@ export const blogData = [
         signupUrl: "https://learn.anuhyadigital.com/introduction-to-cloud-computing-101/"
       },
       {
+        id: 2,
         title: "What is CRM",
         tag: "Salesforce",
         description: "What is CRM Unlock the essence of CRM (Customer Relationship Management) in this concise exploration, unveiling its significance, functions, and benefits.",
         createdDate: "2024-04-04",
         imageUrl: "what-is-crm.jpg",
         signupUrl: "https://learn.anuhyadigital.com/what-is-crm/"
+      },
+      {
+        id: 3,
+        title: "Introduction to salesforce",
+        tag: "Salesforce",
+        description: "Introduction to Salesforce Flows Discover the power of Salesforce Flows to automate processes and boost efficiency.",
+        createdDate: "2024-04-04",
+        imageUrl: "what-is-crm.jpg",
+        signupUrl: "https://learn.anuhyadigital.com/introduction-to-salesforce/"
+      },
+      {
+        id: 4,
+        title: "Introduction to Salesforce Administration 101",
+        tag: "Salesforce",
+        description: "Introduction to Salesforce Flows Discover the power of Salesforce Flows to automate processes and boost efficiency.",
+        createdDate: "2024-04-04",
+        imageUrl: "what-is-crm.jpg",
+        signupUrl: "https://learn.anuhyadigital.com/introduction-to-salesforce-administration-101/"
+      },
+      {
+        id: 5,
+        title: "How to create a Developer Org In Salesforce",
+        tag: "Salesforce",
+        description: "how to create the developer org in salesforce ,Learn to set up a Developer Org in Salesforce with step-by-step guidance, enabling you to explore and develop within the platform efficiently.",
+        createdDate: "2024-04-04",
+        imageUrl: "what-is-crm.jpg",
+        signupUrl: "https://learn.anuhyadigital.com/how-to-create-a-developer-org-in-salesforce/"
+      },
+      {
+        id: 6,
+        title: "Navigation to Salesforce Setup",
+        tag: "Salesforce",
+        description: "Navigation to Salesforce Setup by clicking your user avatar, selecting 'Setup', granting access to administrative settings for customization and configuration.",
+        createdDate: "2024-04-05",
+        imageUrl: "what-is-crm.jpg",
+        signupUrl: "https://learn.anuhyadigital.com/navigation-to-salesforce-setup/"
+      },
+      {
+        id: 7,
+        title: "how to create profile in salesforce",
+        tag: "Salesforce",
+        description: "how to create profile in salesforce Learn how to create profiles in Salesforce, manage permissions, and secure your organization's data.",
+        createdDate: "2024-04-08",
+        imageUrl: "what-is-crm.jpg",
+        signupUrl: "https://learn.anuhyadigital.com/how-to-create-profile-in-salesforce/"
+      },
+      {
+        id: 8,
+        title: "What is role in salesforce",
+        tag: "Salesforce",
+        description: "What is role in salesforce Discover the role concept in Salesforce, its significance, and how it shapes user permissions and access.",
+        createdDate: "2024-04-08",
+        imageUrl: "what-is-crm.jpg",
+        signupUrl: "https://learn.anuhyadigital.com/what-is-role-in-salesforce/"
       }
+
+
+
   ];
   
